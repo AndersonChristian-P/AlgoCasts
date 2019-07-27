80ae27cd186728c0b1bbc67ef85b4f510250addc
Christian Anderson <ca96187@gmail.com>
2019-07-27 17:58:41 -0600

    completed max chars

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,35 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// Given a string return the character that is most commonly used in the string
+
+// Convert the string to an object where the keys of the object are the characters in the string and the values are the number of times that character has been found
+
+function maxChar(str) {
+
+  const charMap = {}
+  let max = 0
+  let maxChar = ""
+
+  for (let char of str) {
+    // for of loop is used to iterate through a string or object
+    if (!charMap[char]) {
+      charMap[char] = 1
+    } else {
+      charMap[char]++
+    }
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char]
+      maxChar = char
+    }
+  }
+
+  return maxChar
+}
+
+console.log(maxChar("Hello There!"))
 
 module.exports = maxChar;
