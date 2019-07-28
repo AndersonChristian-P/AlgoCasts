94f6d1f80e997514736c3aef494013930a6667b9
Christian Anderson <ca96187@gmail.com>
2019-07-27 18:01:23 -0600

    completed palindrome

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,18 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+fizzBuzz = (num) => {
+  for (let i = 0; i < num; i++) {
+    if (i % 15 === 0) {
+      console.log("Fizz Buzz")
+    } else if (i % 3 === 0) {
+      console.log("Fizz")
+    } else if (i % 5 === 0) {
+      console.log("Buzz")
+    } else console.log(i)
+  }
+}
+
+console.log(fizzBuzz(101))
 
 module.exports = fizzBuzz;
